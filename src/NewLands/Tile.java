e950df7f0fac86ba28302800f35c89c73de25e06
xpadrex <xpadrex@gmail.com>
2021-09-12 16:32:08 -0400

    changed walls character

--- src/NewLands/Tile.java
+++ src/NewLands/Tile.java
@@ -5,7 +5,7 @@ import asciiPanel.AsciiPanel;
 
 public enum Tile {
     FLOOR((char)250, AsciiPanel.yellow),    // use a code page middle dot character for floors 
-    WALL((char)178, AsciiPanel.green),     // use a code page block character for walls
+    WALL((char)176, AsciiPanel.green),     // use a code page block character for walls
     BOUNDS('x', AsciiPanel.brightBlack);    // use a 'x' character for boundries
  
     private char glyph;
