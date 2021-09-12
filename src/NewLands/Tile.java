3f401cc1c28a899f89f14dcb9830ba2ee7384889
xpadrex <39248711+xpadrex@users.noreply.github.com>
2021-09-11 21:34:24 -0400

    Add files via upload

---
+++ src/NewLands/Tile.java
@@ -0,0 +1,21 @@
+package NewLands;
+
+import java.awt.Color;
+import asciiPanel.AsciiPanel;
+
+public enum Tile {
+    FLOOR((char)250, AsciiPanel.yellow),    // use a code page middle dot character for floors 
+    WALL((char)178, AsciiPanel.yellow),     // use a code page block character for walls
+    BOUNDS('x', AsciiPanel.brightBlack);    // use a 'x' character for boundries
+ 
+    private char glyph;
+    public char glyph() { return glyph; }
+
+    private Color color;
+    public Color color() { return color; }
+
+    Tile(char glyph, Color color){
+        this.glyph = glyph;
+        this.color = color;
+    }
+}
